65d93e9145ab7552e3decaa2f27708f6c9ee56e4
aleixordeig <aleix.ordeig@gmail.com>
2017-08-23 15:36:03 +0200

    Ng-Repeat

--- public/js/account/favorites/controller.js
+++ public/js/account/favorites/controller.js
@@ -1,4 +1,18 @@
 angular.module('podcast')
 	.controller('GreetingController', ['$scope', function($scope) {
-  		$scope.greeting = 'Hola!';
-	}]);
+  		$scope.greeting = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, 
+    {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, 
+    {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+}]);
+
