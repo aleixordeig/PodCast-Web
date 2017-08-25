f8b54672bf171256fedb18cdd2d7894e7fb18729
aleixordeig <aleix.ordeig@gmail.com>
2017-08-25 10:01:16 +0200

    Min-Height Added Favorites and episodes

--- public/js/favorite/controller.js
+++ public/js/favorite/controller.js
@@ -4,17 +4,20 @@ angular.module('podcast')
     {
       id: 123,
       name: 'Nexus S',
-      url: '/episodes' 
+      url: '/episodes',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
     }, 
     {
       id: 456,
       name: 'Motorola XOOM™ with Wi-Fi',
-      url: 'http://www.skylabcoders.com/es/'
+      url: 'http://www.skylabcoders.com/es/',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
     }, 
     {
       id: 789,
       name: 'MOTOROLA XOOM™',
-      url: 'https://giphy.com/gifs/artestpage-nico-l41lRGoXJJSWyGoFy'
+      url: 'https://giphy.com/gifs/artestpage-nico-l41lRGoXJJSWyGoFy',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
     }
   ];
 	$scope.remove = function(id) { 
