31d8534b1dd65a5d2c02e639c46500ccce95d1a1
aleixordeig <aleix.ordeig@gmail.com>
2017-08-25 14:28:40 +0200

    Button Of Searching Added

---
+++ public/js/search/controller.js
@@ -0,0 +1,38 @@
+angular.module('podcast')
+  .controller('FavouriteController', ['$scope', function($scope) {
+      $scope.favourites = [
+    {
+      id: 123,
+      name: 'Nexus S',
+      url: '/episodes',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
+    }, 
+    {
+      id: 456,
+      name: 'Motorola XOOM™ with Wi-Fi',
+      url: 'http://www.skylabcoders.com/es/',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
+    }, 
+    {
+      id: 789,
+      name: 'MOTOROLA XOOM™',
+      url: 'https://giphy.com/gifs/artestpage-nico-l41lRGoXJJSWyGoFy',
+      text: 'Welcome to the routine of creating of angular and well why are you I still here' 
+    }
+  ];
+  $scope.remove = function(id) { 
+    $scope.favourites = $scope.favourites.filter(function(favourite) {
+       return favourite.id !== id
+    });
+  }
+
+  $scope.hasResults = function () {
+    if($scope.favourites.length > 0){
+      return true
+    } else {
+      false
+    }
+  } 
+
+
+}]);
--- routes/handlers/showSearch.js
+++ routes/handlers/showSearch.js
@@ -1,5 +1,27 @@
 function showSearch (req,res) {
-	res.render('pages/search')
+  console.log(req.query)
+
+  var data = [{
+    title: 'One',
+    description: 'NOW.....',
+    img: 'http',
+    podcast: 'economy'
+  },
+  {
+    title: 'two',
+    description: 'NasOW.....',
+    img: 'http',
+    podcast: 'economy'
+  },
+  {
+    title: 'Three',
+    description: 'NOW.....',
+    img: 'http',
+    podcast: 'economy'
+  },
+  ]
+
+	res.render('pages/search', { data })
 }
 
 module.exports = showSearch
