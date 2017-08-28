787519aaa0e3189c69f8eac4d19638224cde00f9
aleixordeig <aleix.ordeig@gmail.com>
2017-08-28 12:12:43 +0200

    Api Added

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+const dotenv = require('dotenv')
+dotenv.load()
+
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -5,10 +9,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'pug')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 const routesApp = require('./routes/')
 
 app.use(routesApp)
+
 app.listen(PORT)
+
 console.log(`Magic happens at PORT ${PORT}...`)
--- routes/handlers/handlerSearch.js
+++ routes/handlers/handlerSearch.js
@@ -1,45 +1,19 @@
-var Audiosearch = require('audiosearch-client-node');
+const Audiosearch = require('audiosearch-client-node')
 
- 
+const audiosearch = new Audiosearch(process.env.AUDIOSEARCH_APP_ID, process.env.AUDIOSEARCH_SECRET)
 
-function handlerSearch (req,res) {
-  var AUDIOSEARCH_APP_ID = '4e23a0f8f5498359e73caa1e0a3de7fb3821622b8762f0b13399e5553d8504c0'
-  var AUDIOSEARCH_SECRET = '7f32aafcbb0c5e3905f954ce633eea4c66ca6f066fb8ef96e0d0b4768579274d'
-  
-  var audiosearch = new Audiosearch(process.env.AUDIOSEARCH_APP_ID, process.env.AUDIOSEARCH_SECRET);
-  console.log(req.query)
+function handlerSearch(req, res) {
+    console.log(req.query)
 
-  Audiosearch.prototype.searchEpisodes = function (queryString, params) {
-  return this.get('/search/episodes/'+encodeURI(queryString), params);
-  };
-
-  var data = [{
-    title: 'One',
-    description: 'NOW.....',
-    img: 'http',
-    podcast: 'economy'
-  },
-  {
-    title: 'two',
-    description: 'NasOW.....',
-    img: 'http',
-    podcast: 'economy'
-  },
-  {
-    title: 'Three',
-    description: 'NOW.....',
-    img: 'http',
-    podcast: 'economy'
-  }
-  ]
-
-	res.render('pages/search', { 
-    getSearch: function (){
-        return req.query.search
+    res.render('pages/search', {
+        getSearch: function() {
+            return req.query.search
         }
-   })
+        getResult : function () {
+         audiosearch.searchShow('text query').then(function (results) {
+        });
+       }
+    })
 }
 
 module.exports = handlerSearch
-
-
