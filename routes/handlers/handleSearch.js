0b4e16b63bd6ffa046aebd8182e58d26b6a6e8ec
aleixordeig <aleix.ordeig@gmail.com>
2017-08-28 15:41:24 +0200

    Search Podcast Working! ;)

--- routes/handlers/handleSearch.js
+++ routes/handlers/handleSearch.js
@@ -1,28 +1,35 @@
 const Audiosearch = require('audiosearch-client-node')
- 
+
 const audiosearch = new Audiosearch(process.env.AUDIOSEARCH_APP_ID, process.env.AUDIOSEARCH_SECRET)
 
-function handleSearch (req,res) {
-	res.render('pages/search', { 
-    getSearch: function (){
-        return req.query.query
-        },
-    getResult : function () {
-         audiosearch.searchShows(req.query.query).then(function (results) {
-          const _results = results.map(item => {
-             return {
-                name: item.name,
-                // des...
-             }
-          })
-          return console.log(results + '/' + results.title)
+function handleSearch(req, res) {
+
+    audiosearch.searchShows(req.query.query)
+        .then(function(response) {
+            const results = response.results.map(item => {
+                return {
+                    id: item.id,
+                    name: item.title,
+                    description: item.description,
+                    img: item.image_urls.full
+                }
+            })
+            
+            console.log(results)
+
+            res.render('pages/search', {
+                getSearch: function() {
+                    return req.query.query
+                },
+                results: results
+            })
+        })
+        .catch(err => {
+            res.send(err)
         });
-       }
-   })
 }
 
 module.exports = handleSearch
-
 
 // searchShows function added to node_modules!!
 // Audiosearch.prototype.searchShows = function (queryString, params) {
