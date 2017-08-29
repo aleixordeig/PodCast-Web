6de5bf6e7bf48772b729a96f52f188ac96e34c60
aleixordeig <aleix.ordeig@gmail.com>
2017-08-29 12:03:06 +0200

    Episodes to Podcast

--- routes/handlers/handleEpisodes.js
+++ routes/handlers/handleEpisodes.js
@@ -1,4 +1,13 @@
+const Audiosearch = require('audiosearch-client-node')
+const audiosearch = new Audiosearch(process.env.AUDIOSEARCH_APP_ID, process.env.AUDIOSEARCH_SECRET)
+
+
+
 function handleEpisodes (req,res) {
+  console.log(req.params.id)
+  audiosearch.getShow(req.params.id).then(function (results) {
+  console.log(results)
+  });
 	res.render('pages/episodes')
 }
 
