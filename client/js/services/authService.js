7c5cec8b1255b639c90c2a575cb7fb92bb4ccd76
aleixordeig <aleix.ordeig@gmail.com>
2017-09-06 13:21:05 +0200

    Services Added

---
+++ client/js/services/authService.js
@@ -0,0 +1,44 @@
+
+angular.module('podcast')
+  .factory('AuthService', function ($rootScope, $http, StorageService, jwtHelper) {
+    function registerUser (user) {
+      const url = `register`
+      return $http.post(url, user)
+      .then(res => res.data)
+    }
+
+    function isLoggedIn () {
+      const token = StorageService.getToken()
+      if (!token) return false
+      return true
+    }
+
+    function setCredentials (token) {
+      const tokenPayload = jwtHelper.decodeToken(token)
+      $rootScope.loggedUser = tokenPayload.username
+    }
+
+    function doLogin (username, password) {
+      return $http.post('/login', {username, password})
+        .then(res => res.data)
+        .then(data => {
+          StorageService.saveToken(data.token)
+          setCredentials(data.token)
+          return data.success
+        })
+        .catch(data => data.error)
+    }
+
+    function logout () {
+      StorageService.removeToken()
+      delete $rootScope.loggedUser
+    }
+
+    return {
+      registerUser: registerUser,
+      doLogin: doLogin,
+      isLoggedIn: isLoggedIn,
+      setCredentials: setCredentials,
+      logout: logout
+    }
+  })
