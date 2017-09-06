828560d6514997a78d2501d0c7eaee8ee5927a91
aleixordeig <aleix.ordeig@gmail.com>
2017-09-06 13:12:35 +0200

    Reorganize Code

---
+++ client/js/services/StorageService.js
@@ -0,0 +1,16 @@
+angular.module('podcast')
+  .factory('StorageService', function ($window) {
+    function saveToken (token) {
+      $window.localStorage.setItem('authToken', token)
+    }
+
+    function getToken () {
+      return $window.localStorage.getItem('authToken')
+    }
+
+    function removeToken () {
+      $window.localStorage.removeItem('authToken')
+    }
+
+    return { saveToken, getToken, removeToken }
+  })
